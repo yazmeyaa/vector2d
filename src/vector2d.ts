ae3a1f9608c275ac9c4edf22fef69c61ee390f45
Eugene <evgenijantonenkov456@gmail.com>
2024-08-14 18:11:40 +0300

    feat: Add default values to vector constructor

--- src/vector2d.ts
+++ src/vector2d.ts
@@ -7,7 +7,7 @@ export class Vector2D {
    * @param {number} x - The x-coordinate.
    * @param {number} y - The y-coordinate.
    */
-  constructor(x: number, y: number) {
+  constructor(x: number = 0, y: number = 0) {
     this.x = x;
     this.y = y;
   }
